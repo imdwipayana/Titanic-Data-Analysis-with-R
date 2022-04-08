1986234a27f5ad5b52125c62cf0fb36faf8cad08
Eka Dwipayana <86109298+imdwipayana@users.noreply.github.com>
2022-04-08 19:49:15 +0800

    Add files via upload

--- titanic.R
+++ titanic.R
@@ -1,4 +1,13 @@
+#setting working directory
 setwd("~/Video R/Titanic")
 
-train <- read.csv(file="train.csv", stringsAsFactors = FALSE, header = TRUE)
-test <- read.csv(file="test.csv",stringsAsFactors = FALSE,header = TRUE)
+# calling the csv data
+train <- read.csv(file="https://raw.githubusercontent.com/imdwipayana/DATA/main/Data%20Titanic/train.csv", stringsAsFactors = FALSE, header = TRUE)
+test <- read.csv(file="https://raw.githubusercontent.com/imdwipayana/DATA/main/Data%20Titanic/test.csv",stringsAsFactors = FALSE,header = TRUE)
+
+#-------------------------------------------
+# You may check the head and tail by using:
+# head(titanic.train)
+# tail(titanic.train)
+#-------------------------------------------
+head(titanic.train)
